b29ccb2d72df45ee3d2e84dae5a0c4990873d2a7
viettd <viettd@viettds-MacBook-Air.local>
2025-04-22 17:36:55 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,3 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { foo } from 'src/foo';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-}
-bootstrap();
+console.log('Hello World!', foo);
