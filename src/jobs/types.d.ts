3370dfe4a8a922bb8dc941747da14a3f614c096f
viettd <viettd@viettds-MacBook-Air.local>
2025-04-23 15:05:45 +0700

    update

---
+++ src/jobs/types.d.ts
@@ -0,0 +1,10 @@
+export type VerifyOrderJobData = {
+    verify_id: string;
+};
+export type SendRequestData = {
+    method: 'GET' | 'DELETE' | 'HEAD' | 'OPTIONS' | 'POST' | 'PUT' | 'PATCH' | 'PURGE' | 'LINK' | 'UNLINK';
+    url: string;
+    headers?: Record<string, string>;
+    body?: any;
+    params?: any;
+};
