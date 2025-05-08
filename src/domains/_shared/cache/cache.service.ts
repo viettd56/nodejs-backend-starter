c6680371879432e028a7a5325fdf814594736891
viettd <viettd@viettds-MacBook-Air.local>
2025-05-08 16:04:24 +0700

    update

---
+++ src/domains/_shared/cache/cache.service.ts
@@ -0,0 +1,32 @@
+import Redis from 'ioredis';
+import { RedisCache } from './RedisCache';
+import { cacheRepository } from './cache.repository';
+
+const CacheService = () => {
+    const cacheRedis = cacheRepository.createRedis();
+
+    const OtherCache = (ttl: number) => {
+        const cache = new RedisCache(cacheRedis, 'other:', ttl);
+        return {
+            async get(key: string) {
+                const data = await cache.get(key);
+                return data ?? undefined;
+            },
+            set(key: string, value: string) {
+                return cache.set(key, value);
+            },
+            del(key: string) {
+                return cache.del(key);
+            },
+            incr(key: string) {
+                return cache.incr(key);
+            },
+        };
+    };
+
+    return {
+        OtherCache,
+    };
+};
+
+export const cacheService = CacheService();
