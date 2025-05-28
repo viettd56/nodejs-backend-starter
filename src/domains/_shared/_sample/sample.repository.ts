eb79779f8d65b4613aa791f2f655014df5872dcb
viettd <viettd@viettds-MacBook-Air.local>
2025-05-28 10:51:47 +0700

    update

--- src/domains/_shared/_sample/sample.repository.ts
+++ src/domains/_shared/_sample/sample.repository.ts
@@ -1,32 +1,42 @@
 import _ from "lodash";
 import { SampleEntity } from "./sample.entity";
 import { SampleModel } from "src/models/data/Sample.model";
-import { ppid } from "process";
+import { Transaction } from "sequelize";
+import { sequelize } from "src/models/sequelize";
+import { Exception } from "src/helpers/Exception.helper";
 
 const SampleRepository = () => {
     const logic = () => {
         return _.random(-10, 10);
     };
 
-    const update = (data: SampleEntity) => {
+    const update = async (data: SampleEntity, objectModel: SampleModel, transaction?: Transaction) => {
         const {id, name} = data.toObject();
-        return SampleModel.update({
-            name: name,
-        }, {
-            where: {
-                id: id,
-            },
-        });
+        if (id !== objectModel.id){
+            throw new Exception('Data invalid');
+        }
+        objectModel.name = name;
+        return objectModel.save({transaction})
     };
 
     const findById = (id: string) => {
         return SampleModel.findByPk(id);
     };
 
+    const findByIdWithLock = async (id: string, transaction: Transaction) => {
+        return SampleModel.findByPk(id, {
+            transaction,
+            lock: transaction.LOCK.UPDATE
+        });
+    };
+    
+  
+
     return {
         logic,
         update,
         findById,
+        findByIdWithLock,
     };
 };
 
--- src/domains/_shared/_sample/sample.service.ts
+++ src/domains/_shared/_sample/sample.service.ts
@@ -1,5 +1,7 @@
 import { sampleRepository } from "src/domains/_shared/_sample/sample.repository";
 import { SampleEntity } from "./sample.entity";
+import { sequelize } from "src/models/sequelize";
+import { Exception } from "src/helpers/Exception.helper";
 
 const SampleService = () => {
     const logic = async () => {
@@ -14,13 +16,21 @@ const SampleService = () => {
     };
 
     const clearName = async (id: string) => {
-        const sample = await sampleRepository.findById(id);
-        if (!sample) {
-            throw new Error('Sample not found');
-        }
-        const sampleEntity = SampleEntity.fromObjectModel(sample);
-        sampleEntity.clearName();
-        return sampleRepository.update(sampleEntity);
+        await sequelize.transaction(async (t) => {
+            const sample = await sampleRepository.findByIdWithLock(id, t);
+            if (!sample) {
+                await t.rollback();
+                throw new Exception('Sample not found');
+            }
+            
+            const sampleEntity = SampleEntity.fromObjectModel(sample);
+            sampleEntity.clearName();
+            
+            // Cập nhật trong cùng transaction để giữ lock
+            await sampleRepository.update(sampleEntity, sample, t);
+            
+        })
+     
     };
 
     return {
