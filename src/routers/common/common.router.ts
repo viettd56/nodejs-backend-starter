80aaee7f74fc550b13a1b7614d56a1b6efe4dc8e
viettd <viettd@viettds-MacBook-Air.local>
2025-07-23 14:29:22 +0700

    feat: add common route with timestamp response using TypeBox and moment-timezone

--- src/routers/common/common.router.ts
+++ src/routers/common/common.router.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { FastifyPluginCallback } from 'fastify';
 import { HealthCheckRouter } from './healthCheck/healthCheck.router';
+import { Type, TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
+import { commonSchema } from 'src/shared/common/common.schema';
+import moment from 'moment-timezone';
 
 /**
  * Mobile Router provider for NestJS.
@@ -10,6 +13,29 @@ export class CommonRouter {
     constructor(private readonly healthCheckRouter: HealthCheckRouter) {}
 
     public commonRoutes: FastifyPluginCallback = (app) => {
+        app.register((app) => {
+            app.withTypeProvider<TypeBoxTypeProvider>().get(
+                '',
+                {
+                    schema: {
+                        response: {
+                            200: Type.Object({
+                                status: Type.Boolean(),
+                                timestamp: Type.Number(),
+                            }),
+                            default: commonSchema.responseDefault,
+                        },
+                    },
+                },
+                async (req, res) => {
+                    return {
+                        status: true,
+                        timestamp: moment().unix(),
+                    };
+                },
+            );
+        });
+
         app.register(this.healthCheckRouter.healthCheckRoutes, {
             prefix: '/health-check',
         });
